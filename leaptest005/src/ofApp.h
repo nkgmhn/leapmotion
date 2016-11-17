a86f4fcac9100a1820f219b1dce9cdc8a8bbbcaf
nkgmhn <spyair.0412@gmail.com>
2016-11-17 15:29:42 +0900

    Add files via upload

---
+++ leaptest005/src/ofApp.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxLeapMotion2.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+        //void exit();
+    
+        ofxLeapMotion leap;
+        vector<ofxLeapMotionSimpleHand> simpleHands;
+    
+        vector <int> fingersFound;
+        ofEasyCam cam;
+    
+        ofPoint hand[4];
+        ofLight light;
+    
+        ofTrueTypeFont font;
+        bool preHandNum;
+        bool startCount;
+        bool mode;
+        int timeStart;
+        int gravity;
+    
+        float rad, rad2;
+    
+        ofImage image[10];
+    
+        bool drop;
+    
+};
